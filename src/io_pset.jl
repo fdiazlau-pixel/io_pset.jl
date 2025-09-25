16d4d4d52673a66a641d074743b71885fcfbcfa4
Franco Diaz Laura <fdiazlau@asu.edu>
2025-09-25 16:40:44 -0700

    Add data files and update io_pset.jl 	new file:   .DS_Store 	modified:   Manifest.toml 	modified:   Project.toml 	new file:   data/agent_data.csv 	new file:   data/product_data.csv 	modified:   src/io_pset.jl

--- src/io_pset.jl
+++ src/io_pset.jl
@@ -1,5 +1,24 @@
 module io_pset
+ # Installing packages
+using CSV, DataFrames, GLM
 
-# Write your package code here.
+
+agent_original   = CSV.read(joinpath(@__DIR__, "..", "data", "agent_data.csv"), DataFrame)
+describe(agent_original)
+product_original   = CSV.read(joinpath(@__DIR__, "..", "data", "product_data.csv"), DataFrame)
+describe(product_original)
+
+agent_original.income
+
+x = randn(100)
+y = 0.9 .* x + 0.5 * rand(100)
+df = DataFrame(x = x, y = y)
+ols = lm(@formula(y~x), df) # R-style notation
+
+df_product = select(product_original, Not(1))
+
+ols_io = lm(@formula(log(shares)~sugar+prices),df_product)
+
+
 
 end
